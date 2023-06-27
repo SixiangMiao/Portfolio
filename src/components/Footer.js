96b03480c5004f80deba3aac2caa58a863cc29e9
Sixiang Miao <sixiangm@usc.edu>
2023-06-27 08:06:37 -0700

    2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/SixiangMiao"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/sixiang-miao-0968ab231/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
